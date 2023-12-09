277f539f7fff165889e1fdd2b2ec532d3de5df04
lina <linamaulida894@gmail.com>
2023-12-09 14:47:05 +0700

    port

--- configs/Database.js
+++ configs/Database.js
@@ -6,7 +6,8 @@ dotenv.config();
 // Koneksi ke database menggunakan Sequelize
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialect: "mysql"
 });
 
 export default db;
